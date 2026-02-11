12ae16823dc991f3c1c59b4a84f3cbe9aa84f80c
owaismohammad <owaismohammad2515@gmail.com>
2026-02-11 15:33:16 +0530

    added supermemory and made several apis

--- app/prompts.py
+++ app/prompts.py
@@ -2,11 +2,10 @@ BLIP_CUSTOM_PROMPT = "A detailed description of what is going on in this picture
 
 LLM_QUERY_PROMPT = """ YOU ARE CURRENTLY ANSWERING A USERS QUERY BASED ON A VIDEO \n
 below are is the relevant context that has been derived from the video that has been similiar to the prompt use this as reference \n
-{frame_context}
 \n
 \n
-BELOW IS THE CONTEXT DERIVED FROM THE TRANSCRIPT OF THE VIDEO
-{asr_context}
+
+{context}
 THE QUERY ASKED BY THE USER IS GIVEN BELOW:
 {query}
 """
